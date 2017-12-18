b795b317b2e3efc620bd4a445391a296df8b5bb2
martina weidenbaum <martina.weidenbaum@gmail.com>
2017-12-17 19:13:40 -0500

    changed border

--- src/components/image/ErrorPage.js
+++ src/components/image/ErrorPage.js
@@ -3,6 +3,7 @@ import React from 'react'
 const ErrorPage = () => {
 	return(
 	<div className="errorPage">
+		<img className="whale_pic" src={require('./whale.png')}/>
 		<h1>No results found.</h1>
 		<h3> Oh fooey.</h3> 
 		<h3> No worries! Just try another search term ; )</h3>
